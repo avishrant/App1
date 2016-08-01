4988270b7c5535ab03e834012845c28744ce32f9
avish <avish@AHVEE>
2016-08-02 00:08:17 +1000

    Add project files.

---
+++ App1/MainActivity.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using Android.App;
+using Android.Content;
+using Android.Runtime;
+using Android.Views;
+using Android.Widget;
+using Android.OS;
+
+namespace App1
+{
+    [Activity(Label = "App1", MainLauncher = true, Icon = "@drawable/icon")]
+    public class MainActivity : Activity
+    {
+        private Button gbtnsignup;
+        private Button gbtnlogin;
+
+
+        protected override void OnCreate(Bundle bundle)
+        {
+            base.OnCreate(bundle);
+
+            // Set our view from the "main" layout resource
+            SetContentView(Resource.Layout.Main);
+            gbtnsignup = FindViewById<Button>(Resource.Id.btnsignup);
+
+            gbtnsignup.Click += (object sender, EventArgs e) =>
+            {
+                //shows the fragment display
+                FragmentTransaction trans = FragmentManager.BeginTransaction();
+                dialogsignup signupdialog = new dialogsignup();
+                signupdialog.Show(trans, "fragment dialog");
+            };
+
+            gbtnlogin = FindViewById<Button>(Resource.Id.btnlogin);
+
+            gbtnlogin.Click += (object sender, EventArgs e) =>
+            {
+                //login fragment
+                FragmentTransaction trans = FragmentManager.BeginTransaction();
+                dialoglogin logindialog = new dialoglogin();
+                logindialog.Show(trans, "fragment dialog");
+            };
+            
+        }
+        
+    }
+}
+
---
+++ App1/Resources/dialoglogin.cs
@@ -0,0 +1,32 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+using Android.App;
+using Android.Content;
+using Android.OS;
+using Android.Runtime;
+using Android.Views;
+using Android.Widget;
+
+namespace App1
+{
+    class dialoglogin : DialogFragment
+    {
+        public override View OnCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState)
+        {
+            base.OnCreateView(inflater, container, savedInstanceState);
+
+            var view = inflater.Inflate(Resource.Layout.loginfrag, container, false);
+            return view;
+
+        }
+        public override void OnActivityCreated(Bundle savedInstanceState)
+        {
+            Dialog.Window.RequestFeature(WindowFeatures.NoTitle); //invisible title bar
+            base.OnActivityCreated(savedInstanceState);
+            Dialog.Window.Attributes.WindowAnimations = Resource.Style.signupanimation; //setting animation
+        }
+    }
+}
