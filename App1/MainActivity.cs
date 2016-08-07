6fd34da8073e0b6a3e0a899a0864e1a28a91a2a0
avishrant <avishrant.stha@gmail.com>
2016-08-07 20:45:10 +1000

    Add files via upload

--- App1/MainActivity.cs
+++ App1/MainActivity.cs
@@ -1,4 +1,6 @@
 ﻿using System;
+using System.IO;
+using System.Threading;
 using Android.App;
 using Android.Content;
 using Android.Runtime;
@@ -6,43 +8,72 @@ using Android.Views;
 using Android.Widget;
 using Android.OS;
 
+
 namespace App1
 {
-    [Activity(Label = "App1", MainLauncher = true, Icon = "@drawable/icon")]
+    [Activity()]
     public class MainActivity : Activity
     {
-        private Button gbtnsignup;
-        private Button gbtnlogin;
+        private Button gbtnsignup; //global variable
+        private Button gbtnlogin; //global variable
 
 
         protected override void OnCreate(Bundle bundle)
         {
             base.OnCreate(bundle);
+            SetContentView(Resource.Layout.Main);
 
             // Set our view from the "main" layout resource
-            SetContentView(Resource.Layout.Main);
-            gbtnsignup = FindViewById<Button>(Resource.Id.btnsignup);
 
-            gbtnsignup.Click += (object sender, EventArgs e) =>
-            {
-                //shows the fragment display
-                FragmentTransaction trans = FragmentManager.BeginTransaction();
-                dialogsignup signupdialog = new dialogsignup();
-                signupdialog.Show(trans, "fragment dialog");
-            };
 
-            gbtnlogin = FindViewById<Button>(Resource.Id.btnlogin);
-
+            gbtnlogin = FindViewById<Button>(Resource.Id.btnlogin);//login button, first
             gbtnlogin.Click += (object sender, EventArgs e) =>
             {
                 //login fragment
                 FragmentTransaction trans = FragmentManager.BeginTransaction();
                 dialoglogin logindialog = new dialoglogin();
                 logindialog.Show(trans, "fragment dialog");
+                logindialog.gOnDialogLogin += logindialog_gOnDialogLogin; //need void dialoglogin_gOnDialogLogin{see below}
+
             };
             
+            gbtnsignup = FindViewById<Button>(Resource.Id.btnsignup);//signup button, first screen
+
+            gbtnsignup.Click += (object sender, EventArgs e) =>      //need to subscribe to this event
+            {
+                //signup fragment
+                FragmentTransaction trans = FragmentManager.BeginTransaction();//using events, do not want to use methods
+                dialogsignup signupdialog = new dialogsignup();
+                signupdialog.Show(trans, "fragment dialog");
+
+                signupdialog.gOnDialogSign += dialogsignup_gOnDialogSign;
+            };
+        }
+        void logindialog_gOnDialogLogin(object sender, OnDialogLogin e)
+        {
+           
+        }
+
+       
+        void dialogsignup_gOnDialogSign(object sender, OnDialogSignup e)
+        {
+            // try making a global variable to instantiate the email and password fields, and try after that.
+            //if it doesn't work, make a dummy thread, and proceed. 
+        
+
+
+            using (StreamWriter writer = new StreamWriter("info.txt"))//Thread thread = new Thread(dummy);
+                                                                //thread.start();
+            //private void dummy() {Thread.Sleep(3000);}
+            {
+                writer.Write(e.Email);
+                writer.Write(e.FirstName);
+                writer.Write(e.Password);
+            }
+        }
+        }
         }
         
-    }
-}
+    
+
 
--- App1/dialogsignup.cs
+++ App1/dialogsignup.cs
@@ -12,16 +12,70 @@ using Android.Widget;
 
 namespace App1
 {
-    class dialogsignup : DialogFragment
+    public class OnDialogSignup : EventArgs
     {
+        private string gFristName;
+        private string gEmail;
+        private string gPassword;
+       
+        public string FirstName
+        {
+            get { return gFristName; }
+            set { gFristName = value; }
+        }
+        public string Email
+        {
+            get { return gEmail; }
+            set { gEmail = value; }
+        }
+        public string Password
+        {
+            get { return gPassword; }
+            set { gPassword = value; }
+        }
+
+        public OnDialogSignup ( string firstname, string email, string password) : base()//sign up button in the fragment
+        {
+            FirstName = firstname;
+            Email = email;
+            Password = password;
+        }
+    }
+    class dialogsignup : DialogFragment
+    
+    {
+        private EditText gTxtFirstName;
+        private EditText gTxtEmail;
+        private EditText gTxtPassword;
+        private Button gbtndialogsignup;
+
+        public event EventHandler<OnDialogSignup> gOnDialogSign;
+
         public override View OnCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState)
         {
             base.OnCreateView(inflater, container, savedInstanceState);
 
             var view = inflater.Inflate(Resource.Layout.signupfrag, container, false);
+
+            gTxtFirstName = view.FindViewById<EditText>(Resource.Id.txtFirstName);
+            gTxtEmail = view.FindViewById<EditText>(Resource.Id.txtEmail);
+            gTxtPassword = view.FindViewById<EditText>(Resource.Id.txtPassword);
+            gbtndialogsignup = view.FindViewById<Button>(Resource.Id.btndialogsignup);
+
+            gbtndialogsignup.Click += gbtndialogsignup_Click;
+
             return view;
 
         }
+
+        void gbtndialogsignup_Click(object sender, EventArgs e)
+        {
+            //fragment signup button clicky clicky
+            gOnDialogSign.Invoke(this, new OnDialogSignup(gTxtFirstName.Text, gTxtEmail.Text, gTxtPassword.Text));//invoking event, can also use OnDialogSing.Invoke
+            this.Dismiss();
+        }
+
+
         public override void OnActivityCreated(Bundle savedInstanceState)
         {
             Dialog.Window.RequestFeature(WindowFeatures.NoTitle); //invisible title bar
